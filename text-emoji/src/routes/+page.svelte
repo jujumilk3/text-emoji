35db31b4ebfaa393753e16a4f43a954e36d14352
jujumilk3 <jujumilk3@gmail.com>
2025-03-09 00:23:33 +0900

    add hello world

--- text-emoji/src/routes/+page.svelte
+++ text-emoji/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	let text = '';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>Hello World</h2>
